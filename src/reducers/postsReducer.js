4bd8ba03a411865facd66d3301c63ed14a6a42cc
Marcin Kegel <kegelmarcin@gmail.com>
2021-01-29 18:01:13 +0100

    reducer added, display of list of posts done

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,18 @@
-export default () => {
-    return 123
+export default (state = [], action) => {
+    if (action.type === 'FETCH_POSTS') {
+        return action.payload
+    }
+
+    return state;
 };
+
+// or it could be like that with a switch
+//
+//     export default (state = [], action) => {
+//         switch (action.type) {
+//             case 'FETCH_POSTS':
+//                 return action.payload;
+//             default:
+//                 return state;
+//         }
+//     };
